3703a09ea0fafedf11a295635a1b58b745f8cc8d
Hamza DAMIRI <hamza.damiri@gmail.com>
2024-02-18 11:47:16 +0100

    Instenciation de l'objet "metier" avec constructeur.

--- src/main/java/presentation/Presentation.java
+++ src/main/java/presentation/Presentation.java
@@ -2,14 +2,15 @@ package presentation;
 
 import dao.DaoImpl;
 import dao.IDao;
+import extension.DaoImplV2;
 import metier.IMetier;
 import metier.MetierImpl;
 
 public class Presentation {
     public static void main(String[] args) {
-        MetierImpl metier = new MetierImpl();
         IDao d = new DaoImpl();
-        metier.setDao(d);
+        MetierImpl metier = new MetierImpl(d);
+        //metier.setDao(d);
         System.out.println("Res = " + metier.calcul());
     }
 }
